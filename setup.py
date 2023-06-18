b1035ff4822c5b0eb9b81eb8ebc1ab3ea04b339f
smheidrich <smheidrich@weltenfunktion.de>
2023-06-18 16:35:39 +0200

    Initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="dummy",
+    version="0.1.0",
+    packages=["dummy"],
+    zip_safe=False,
+    description="Dummy package to show this or that",
+    readme="README.md",
+)
